1330595393e97b9f2196e22e916f4da5e745933c
Mohammad-asimm8601 <mohammadasim84924@gmail.com>
2025-12-16 23:22:36 +0530

    Initial commit

---
+++ Thread/TestA.java
@@ -0,0 +1,18 @@
+package Thread;
+
+public class TestA {
+    public static void main(String[] args) {
+        Counter counter = new Counter();
+        MyThread t1 = new MyThread(counter);
+        MyThread t2 = new MyThread(counter);
+        t1.start();
+        t2.start();
+        try {
+            t1.join();
+            t2.join();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        System.out.println(counter.getCount());
+    }
+}
