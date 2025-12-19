ad2579e14c58ff86ac47719853f3118311b4eb1c
Mohammad-asimm8601 <mohammadasim84924@gmail.com>
2025-12-19 12:16:31 +0530

    Initial commit

--- questions/StudentRegistrationForm.java
+++ questions/StudentRegistrationForm.java
@@ -76,5 +76,6 @@ public class StudentRegistrationForm extends Frame implements ActionListener {
         
     }
 }
+
     
 
---
+++ questions/index.ts
@@ -0,0 +1,2 @@
+// This is a simple ts file
+// This is a simple ts file
